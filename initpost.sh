55bf05d297bff3391aeb0dd3aa05b945db9f508f
Marcos Oliveira <marcos@terminalroot.com.br>
2019-07-26 14:47:21 -0300

    automatic commit

--- initpost.sh
+++ initpost.sh
@@ -1,161 +1,71 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# ------------------------------------------------------------------------------
-#
-# Program: initpost.sh
-# Author:  Vitor Britto changed by Marcos Oliveira
-# Description: script to create an initial structure for my posts.
-#
-# Usage: ./initpost.sh [options] <post name>
-#
-# Options:
-#   -h, --help        output instructions
-#   -c, --create      create post
-#
-# Alias: alias ipost="bash ~/path/to/script/initpost.sh"
-#
-# Example:
-#   ./initpost.sh -c How to replace strings with sed
-#
-# Important Notes:
-#   - This script was created to generate new markdown files for my blog.
-#
-# ------------------------------------------------------------------------------
+# author: Marcos Oliveira <terminalroot.com.br>
+# describe: Script to create an initial structure for posts in Jekyll
+# version: 1.0
+# license: MIT License
 
+_usage(){
+  cat <<EOF
+usage: ${0##*/} options [title]
+  
+  Options:
+    -c [title]    Show channel title
+    -h            Print this help message
+    -v            Print version
 
-# ------------------------------------------------------------------------------
-# | VARIABLES                                                                  |
-# ------------------------------------------------------------------------------
-
-# CORE: Do not change these lines
-# ----------------------------------------------------------------
-POST_TITLE="${@:2:$(($#-1))}"
-POST_NAME="$(echo ${@:2:$(($#-1))} | sed -e 's/ /-/g' | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")"
-POST_NAME=$( echo $POST_NAME | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚüÜçÇ/aAaAaAaAeEeEiIoOoOoOuUuUcC/' | tr -d ',.;:');
-CURRENT_DATE="$(date +'%Y-%m-%d')"
-TIME=$(date +"%T")
-FILE_NAME="${CURRENT_DATE}-${POST_NAME}.md"
-# ----------------------------------------------------------------
-
-
-# SETTINGS: your configuration goes here
-# ----------------------------------------------------------------
-
-# Set your destination folder
-BINPATH=$(cd `dirname $0`; pwd)
-POSTPATH="${BINPATH}/_posts"
-DIST_FOLDER="$POSTPATH"
-
-# Set your blog URL
-BLOG_URL="http://terminalroot.com.br"
-
-# Set your assets URL
-ASSETS_URL="img/"
-# ----------------------------------------------------------------
-
-
-
-# ------------------------------------------------------------------------------
-# | UTILS                                                                      |
-# ------------------------------------------------------------------------------
-
-# Header logging
-e_header() {
-    printf "$(tput setaf 38)→ %s$(tput sgr0)\n" "$@"
-}
-
-# Success logging
-e_success() {
-    printf "$(tput setaf 76)✔ %s$(tput sgr0)\n" "$@"
-}
-
-# Error logging
-e_error() {
-    printf "$(tput setaf 1)✖ %s$(tput sgr0)\n" "$@"
-}
-
-# Warning logging
-e_warning() {
-    printf "$(tput setaf 3)! %s$(tput sgr0)\n" "$@"
-}
-
-
-
-# ------------------------------------------------------------------------------
-# | MAIN FUNCTIONS                                                             |
-# ------------------------------------------------------------------------------
-
-# Everybody need some help
-initpost_help() {
-
-cat <<EOT
-------------------------------------------------------------------------------
-INIT POST - A shortcut to create an initial structure for my posts.
-------------------------------------------------------------------------------
-Usage: ./initpost.sh [options] <post name>
-Options:
-  -h, --help        output instructions
-  -c, --create      create post
-Example:
-  ./initpost.sh -c How to replace strings with sed
-Important Notes:
-  - This script was created to generate new text files to my blog.
-Copyright (c) Vitor Britto
-Licensed under the MIT license.
-------------------------------------------------------------------------------
-EOT
+* Script in development
+EOF
 
 }
 
-# Initial Content
-initpost_content() {
+_skell(){
 
-echo "---"
-echo "layout: post"
-echo "title: \"${POST_TITLE}\""
-echo "date: ${CURRENT_DATE} ${TIME}"
-echo "image: '/assets/img/'"
-echo "description:"
-echo "main-class:"
-echo "tags:"
-echo "---"
+    _DATE=$(date +%Y-%m-%d)
+    _TIME=$(date +%H:%M:%S)
+
+    echo "---"
+    echo "layout: post"
+    echo "title: \"${1}\""
+    echo "date: ${_DATE} ${_TIME}"
+    echo "image: '/assets/img/'"
+    echo "description:"
+    echo "main-class:"
+    echo "tags:"
+    echo "---"
+    echo
+    echo '
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Informat -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-2838251107855362"
+         data-ad-slot="2327980059"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    '
+}
+
+_initpost(){
+
+    _PADRAO=$(date +%Y-%m-%d)
+    _URL=$(echo $1 | sed 's/+/-/g' | tr A-Z a-z | tr -d '!@#$%&*()_<>}{~?^/:\"' | tr ' ' '-' | sed 's/--//g' |
+          sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚüÜçÇ/aAaAaAaAeEeEiIoOoOoOuUuUcC/' | tr -d ',.;:' | sed 's/-$//g')
+    _skell "$1" > "_posts/$_PADRAO-$_URL.md"
+    echo -e "\e[36;1m➜ File created successfully!\n\e[37;1m_posts/$_PADRAO-$_URL.md\e[m"
 
 }
 
-# Create file
-initpost_file() {
-    if [ ! -f "$FILE_NAME" ]; then
-        e_header "Creating template..."
-        initpost_content > "${DIST_FOLDER}/${FILE_NAME}"
-        e_success "Initial post successfully created!"
-    else
-        e_warning "File already exist."
-        exit 1
-    fi
-
-}
-
-
-
-# ------------------------------------------------------------------------------
-# | INITIALIZE PROGRAM                                                         |
-# ------------------------------------------------------------------------------
-
-main() {
-
-    # Show help
-    if [[ "${1}" == "-h" || "${1}" == "--help" ]]; then
-        initpost_help ${1}
-        exit
-    fi
-
-    # Create
-    if [[ "${1}" == "-c" || "${1}" == "--create" ]]; then
-        initpost_file $*
-        exit
-    fi
-
-}
-
-# Initialize
-main $*
+while getopts c:vh PARAM
+do 
+	case "$PARAM" in
+		c) _initpost "$OPTARG" ;;
+		h) _usage && exit 0   ;;
+		v) sed -n '/^#.*version/p' $0 | sed 's/^# //' ;;
+		*) _usage && exit 1   ;;
+	esac
+# shift $(( OPTIND - 1 ))
+done
