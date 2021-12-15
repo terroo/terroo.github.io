5832a0cba71a6c615f31a494340c763167166f37
Marcos Oliveira <marcos@terminalroot.com.br>
2021-12-15 03:52:49 -0300

    checkURL

--- assets/js/checkUrl.js
+++ assets/js/checkUrl.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-  $("a[href^='http']:not([href^='https://terminalroot.com/'])").attr("target","_blank");
-  $("a[href^='http']:not([href^='https://terminalroot.com/'])").attr("rel","noopener noreferrer");
+  $("a[href^='http']:not([href^='https://terminalroot.com'])").attr("target","_blank");
+  $("a[href^='http']:not([href^='https://terminalroot.com'])").attr("rel","noopener noreferrer");
 });
