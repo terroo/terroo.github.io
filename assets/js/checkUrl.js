b7c9cbe40de47b86ab3ae2d1530629e944aebe54
Marcos Oliveira <marcos@terminalroot.com.br>
2021-12-15 14:21:23 -0300

    pre code a points-events none

--- assets/js/checkUrl.js
+++ assets/js/checkUrl.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+  $("pre").removeAttr("href");
+  $("pre code").removeAttr("href");
+
   $("a[href^='http']:not([href^='https://terminalroot.com'])").attr("target","_blank");
   $("a[href^='http']:not([href^='https://terminalroot.com'])").attr("rel","noopener noreferrer");
 });
